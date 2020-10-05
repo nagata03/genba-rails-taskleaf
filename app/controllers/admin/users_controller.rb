4977c5f7b51aab7cf031f18e2cffa8dcc791cbe7
nagata03 <nagata.job@gmail.com>
2020-10-05 13:22:23 +0900

    ユーザー一覧画面の実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,8 @@
 class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -17,9 +21,6 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
-  end
-
-  def index
   end
 
   private
