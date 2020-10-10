7bd139dfc8f02b8ddee4f468d026439cdcebb5a5
nagata03 <nagata.job@gmail.com>
2020-10-10 10:57:49 +0900

    ユーザー詳細画面を実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,6 +3,10 @@ class Admin::UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -18,9 +22,6 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
-  end
-
-  def show
   end
 
   private
